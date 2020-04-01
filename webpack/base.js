07364492dc7b8a32ecd7bd7b5bbdaac57439b0f6
Marvin Helstein <m_helstein@hotmail.com>
2020-04-01 16:21:40 +0300

    Cleaned up packages, fixed webpack

--- webpack/base.js
+++ webpack/base.js
@@ -9,9 +9,11 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.tsx?$/,
-        use: 'ts-loader',
-        exclude: /node_modules/
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
       },
       {
         test: [/\.vert$/, /\.frag$/],
